8ad460b6706a21456570e5bf78fd8d93766862c7
noerten <darkstria@gmail.com>
2016-03-27 01:20:23 +0300

    replies to comments

--- comments/models.py
+++ comments/models.py
@@ -5,10 +5,14 @@ from django.contrib.contenttypes.models import ContentType
 
 
 class CommentManager(models.Manager):
+    def all(self):
+        qs = super(CommentManager, self).filter(parent=None)
+        return qs
+
     def filter_by_instance(self, instance):
         content_type = ContentType.objects.get_for_model(instance.__class__)#(Post)
         obj_id = instance.id
-        qs = super(CommentManager, self).filter(content_type=content_type, object_id=obj_id)
+        qs = super(CommentManager, self).filter(content_type=content_type, object_id=obj_id).filter(parent=None)
         return qs
 
 class Comment(models.Model):
@@ -17,7 +21,21 @@ class Comment(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
+    parent = models.ForeignKey('self', null=True, blank=True)
     added = models.DateTimeField(auto_now_add=True)
     objects = CommentManager()
+
+  #  class Meta:
+   #     ordering = ['-added']
+
     def __str__(self):
         return str(self.user.username)
+
+    def children(self):
+        return Comment.objects.filter(parent=self)
+
+    @property
+    def is_parent(self):
+        if self.parent is not None:
+            return False
+        return True
