e2eb3be4dfc81c51d4e46bc38c441ee9400709f1
noerten <darkstria@gmail.com>
2016-03-26 19:22:31 +0300

    crispy forms

--- myblog/settings.py
+++ myblog/settings.py
@@ -39,10 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'posts',
     #3d party
+    'crispy_forms',
     'markdown_deux',
     'pagedown',
 
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
--- posts/forms.py
+++ posts/forms.py
@@ -8,11 +8,10 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = {
-
             'title',
             'content',
             'image',
             'draft',
-            'publish'
+            'publish',
 
         }
